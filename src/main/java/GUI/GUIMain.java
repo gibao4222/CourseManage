71a01f57c453e89eb430bad5d56a3e0b3e21faff
khacduy <2002nguyenkhacduy@gmail.com>
2023-02-26 13:52:31 +0700

    chinh layout GUI quan ly khoa hoc

--- src/main/java/GUI/GUIMain.java
+++ src/main/java/GUI/GUIMain.java
@@ -5,6 +5,9 @@
 package GUI;
 
 import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 
 /**
  *
@@ -17,8 +20,7 @@ public class GUIMain extends javax.swing.JFrame {
      */
     public GUIMain() {
         initComponents();
-        setBackground(new Color(0,0,0,0));
-
+//        setBackground(new Color(0,0,0,0));
     }
 
     /**
@@ -32,21 +34,54 @@ public class GUIMain extends javax.swing.JFrame {
 
         panelBorder2 = new Custom.PanelBorder();
         menu1 = new Custom.Menu();
+        jLabel1 = new javax.swing.JLabel();
+        PanelSwitch = new javax.swing.JDesktopPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
+        setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+        setName("frameMain\n"); // NOI18N
+        setResizable(false);
 
         panelBorder2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Quản lý khóa học");
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout menu1Layout = new javax.swing.GroupLayout(menu1);
         menu1.setLayout(menu1Layout);
         menu1Layout.setHorizontalGroup(
             menu1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 227, Short.MAX_VALUE)
+            .addGroup(menu1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
+                .addContainerGap())
         );
         menu1Layout.setVerticalGroup(
             menu1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGroup(menu1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(650, Short.MAX_VALUE))
+        );
+
+        PanelSwitch.setPreferredSize(new java.awt.Dimension(1056, 720));
+
+        javax.swing.GroupLayout PanelSwitchLayout = new javax.swing.GroupLayout(PanelSwitch);
+        PanelSwitch.setLayout(PanelSwitchLayout);
+        PanelSwitchLayout.setHorizontalGroup(
+            PanelSwitchLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1056, Short.MAX_VALUE)
+        );
+        PanelSwitchLayout.setVerticalGroup(
+            PanelSwitchLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout panelBorder2Layout = new javax.swing.GroupLayout(panelBorder2);
@@ -55,11 +90,13 @@ public class GUIMain extends javax.swing.JFrame {
             panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBorder2Layout.createSequentialGroup()
                 .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 1053, Short.MAX_VALUE))
+                .addGap(0, 0, 0)
+                .addComponent(PanelSwitch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         panelBorder2Layout.setVerticalGroup(
             panelBorder2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(PanelSwitch, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -77,6 +114,24 @@ public class GUIMain extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        // TODO add your handling code here:
+        clickListenerLeftMenu(new QuanLyKhoaHocGUI());
+    }//GEN-LAST:event_jLabel1MouseClicked
+
+    private void clickListenerLeftMenu(JPanel panelPara){
+        //hàm chung để chuyển cái GUI con
+        
+        
+        //Bỏ hết các GUI con cũ trước khi add GUI con mới vào
+        PanelSwitch.removeAll();
+        //lấy Bounds của jDesktopPanel để set Bound cho GUI con
+        panelPara.setBounds(0,0,PanelSwitch.getPreferredSize().width,PanelSwitch.getPreferredSize().height);
+        // làm cho GUI con hiển thị
+        panelPara.setVisible(true);
+        // add GUI con vào jDesktopPanel
+        PanelSwitch.add(panelPara);
+    }
     /**
      * @param args the command line arguments
      */
@@ -113,6 +168,8 @@ public class GUIMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane PanelSwitch;
+    private javax.swing.JLabel jLabel1;
     private Custom.Menu menu1;
     private Custom.PanelBorder panelBorder2;
     // End of variables declaration//GEN-END:variables
